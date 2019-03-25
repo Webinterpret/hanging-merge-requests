360bc5a55ca777322da776b7dbf3bfb2f08b0104
matee <matee@matee.net>
2019-03-25 10:44:20 +0100

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_file(filename):
 
 setup(
     name='hanging-merge-requests',
-    version='1.0.0-rc5',
+    version='1.0.0',
     packages=find_packages(include=('hmr*', )),
     include_package_data=True,
     description='Emojiful daily summaries of open merge requests directly in your Slack',
